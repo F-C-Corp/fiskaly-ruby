53fd8bc29afe631665b547ebd8371047df6b8ecc
Christian Zacharias <christian.zacharias@gmail.com>
2024-06-18 16:03:37 +0200

    fix for scu and cash registers

--- lib/fiskaly_ruby/sign_at/cash_registers/create.rb
+++ lib/fiskaly_ruby/sign_at/cash_registers/create.rb
@@ -20,40 +20,14 @@ module FiskalyRuby
           @cash_register_id = cash_register_id
         end
 
-        #     Required payload attributes
-        #
-        # @return [Array] Set of required attributes
-        def required_payload_attributes
-          %i(legal_entity_id)
-        end
          # Execute SCU creation
         #
         # @return [Hash] Formatted response informations
         def call
-          _validate_params
           # PUT https://rksv.fiskaly.com/api/v1/cash-register/{cash_register_id}
           response = self.class.put("/cash-register/#{cash_register_id}", headers: headers, body: body)
 
           handle_response(response)
-        end
-
-        private
-
-        # Validate parameters
-        #
-        # @return NilClass
-        def _validate_params
-          _validate_legal_entity_id
-          _validate_metadata
-        end
-
-        # Validate name parameter
-        #
-        # @return NilClass
-        def _validate_legal_entity_id
-          legal_entity_id = payload[:legal_entity_id]
-
-          raise "Invalid legal_entity_id for: #{legal_entity_id.inspect}" unless legal_entity_id.is_a?(Object)
         end
       end
     end
--- lib/fiskaly_ruby/sign_at/signature_creation_units/create.rb
+++ lib/fiskaly_ruby/sign_at/signature_creation_units/create.rb
@@ -11,6 +11,13 @@ module FiskalyRuby
       class Create < Base
         attr_reader :scu_id
 
+        #     Required payload attributes
+        #
+        # @return [Array] Set of required attributes
+        def required_payload_attributes
+          %i(legal_entity_id)
+        end
+
         def initialize(token:, scu_id:, payload: {})
           super(token: token, payload: payload)
 
@@ -21,10 +28,30 @@ module FiskalyRuby
         #
         # @return [Hash] Formatted response informations
         def call
+          _validate_params
           # PUT https://rksv.fiskaly.com/api/v1/signature-creation-unit/{signature_creation_unit_id}
           response = self.class.put("/signature-creation-unit/#{scu_id}", headers: headers, body: body)
 
           handle_response(response)
+        end
+
+        private
+
+        # Validate parameters
+        #
+        # @return NilClass
+        def _validate_params
+          _validate_legal_entity_id
+          _validate_metadata
+        end
+
+        # Validate name parameter
+        #
+        # @return NilClass
+        def _validate_legal_entity_id
+          legal_entity_id = payload[:legal_entity_id]
+
+          raise "Invalid legal_entity_id for: #{legal_entity_id.inspect}" unless legal_entity_id.is_a?(Object)
         end
       end
     end
